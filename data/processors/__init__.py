3f38a7fa33798cb21b3af6100575924596743b41
kveje <kasper@vrls.dk>
2025-03-15 20:34:53 +0100

    updated processor structure

--- data/processors/__init__.py
+++ data/processors/__init__.py
@@ -4,8 +4,18 @@ This module contains data processing functionality for feature engineering,
 normalization, and universe selection.
 """
 
-from data.processors.feature_engineering import FeatureEngineer
-from data.processors.normalization import Normalizer
-from data.processors.universe_selection import UniverseSelector
+from data.processors.normalization import NormalizeProcessor
+from data.processors.technical_indicator import TechnicalIndicatorProcessor
+from data.processors.turbulence import TurbulenceProcessor
+from data.processors.vix import VIXProcessor
+from data.processors.processor import BaseProcessor
+from data.processors.normalization import NormalizeProcessor
 
-__all__ = ["FeatureEngineer", "Normalizer", "UniverseSelector"]
+__all__ = [
+    "NormalizeProcessor",
+    "TechnicalIndicatorProcessor",
+    "TurbulenceProcessor",
+    "VIXProcessor",
+    "BaseProcessor",
+    "NormalizeProcessor",
+]
