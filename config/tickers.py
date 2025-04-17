e40c31adfdbedde945fd3655840ca0a560308ea0
kveje <kasper@vejejakobsen.dk>
2025-04-17 18:07:56 +0200

    filtered DOW_30_TICKER to exclude 'DOW' from the list

--- config/tickers.py
+++ config/tickers.py
@@ -1,5 +1,5 @@
 from finrl import config_tickers as cfg_tickers
 
-DOW_30_TICKER = cfg_tickers.DOW_30_TICKER
+DOW_30_TICKER = [i for i in cfg_tickers.DOW_30_TICKER if i != "DOW"]
 NASDAQ_100_TICKER = cfg_tickers.NAS_100_TICKER
 SP_500_TICKER = cfg_tickers.SP_500_TICKER
